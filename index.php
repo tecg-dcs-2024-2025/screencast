2aae363d257700b672185242543b2c02297912cb
Dominique Vilain <dominique.vilain@hepl.be>
2025-02-10 11:09:57 +0100

    add favicons and update meta

--- index.php
+++ index.php
@@ -7,12 +7,29 @@ session_start() ?>
         <head>
             <meta charset="utf-8">
             <meta name="description"
-                  content="Description">
+                  content="Vous avez perdu un animal ? Signalez-le nous via ce formulaire">
             <meta name="keywords"
-                  content="Keywords">
+                  content="animal, animal perdu, déclaration">
             <meta name="author"
-                  content="Author">
+                  content="Dominique Vilain">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible"
+                  content="ie=edge">
             <title>J’ai perdu mon animal</title>
+            <link rel="apple-touch-icon"
+                  sizes="180x180"
+                  href="/apple-touch-icon.png">
+            <link rel="icon"
+                  type="image/png"
+                  sizes="32x32"
+                  href="/favicon-32x32.png">
+            <link rel="icon"
+                  type="image/png"
+                  sizes="16x16"
+                  href="/favicon-16x16.png">
+            <link rel="manifest"
+                  href="/site.webmanifest">
         </head>
         <body>
             <h1>Déclaration de perte d'animal</h1>
