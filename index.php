7e05118d9bf4235e243e9497222b9c890694bbac
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-03-09 22:14:21 +0100

    Implement CSRF attack detection

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ session_start();
 
 $countries = require './config/countries.php';
 $pet_types = require './config/pet_types.php';
+require './core/helpers/functions.php';
 ?>
     <!DOCTYPE html>
     <html lang="fr">
@@ -41,6 +42,9 @@ $pet_types = require './config/pet_types.php';
             <h1>Déclaration de perte d'animal</h1>
             <form action="/process.php"
                   method="post">
+                <?php if ($_SERVER['REQUEST_METHOD']=== 'POST' || $_SERVER['REQUEST_METHOD']=== 'post'):?>
+                    <input type="hidden" name="csrf_token" value="<?php csrf();?>">
+                <?php endif;?>
                 <fieldset>
                     <legend>Vos coordonnées</legend>
                     <div class="fields">
