04cd70edd5a8cec5873d44c9bf9bd34b4e28376f
FournyAmandine <amandine.fourny@student.hepl.be>
2025-03-18 16:43:27 +0100

    Update Validator.php

--- core/Validator.php
+++ core/Validator.php
@@ -68,6 +68,7 @@ class Validator
 
     public static function in_collection(string $field_name, string $collection_name, array $collection): bool
     {
+        $collection = require __DIR__.'/../config/'.$collection_name.'.php';
         if (array_key_exists($field_name, $_REQUEST) &&
             trim($_REQUEST[$field_name]) !== '' &&
             !array_key_exists($_REQUEST[$field_name], $collection)) {
@@ -80,19 +81,18 @@ class Validator
         return true;
     }
 
-    public static function check(array $rules)
+    public static function check(array $constraints): void
     {
-        self::parse_constraints($rules);
-
-        //Analyser les contraintes définies dans l’array
-        //À partir de cette analyse appeler les méthodes de validation correspondantes
-
+        try{
+            self::parse_constraints($constraints);
+        } catch(ValidationRulesNotFoundException $exception){
+            die($exception->getMessage());
+        }
         if (isset($_SESSION['errors'])) {
             $_SESSION['old'] = $_REQUEST;
             header('Location: /index.php');
             exit;
         }
-        return true;
     }
 
     private static function parse_constraints(array $constraints): void
