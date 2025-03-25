16e6084cab0cd6ac2caadc9d25e373318443577b
JeanRoyen <jean.royen@student.hepl.be>
2025-03-25 12:24:01 +0100

    Improved Response class to consider various types of responses

--- core/Response.php
+++ core/Response.php
@@ -6,10 +6,26 @@ use JetBrains\PhpStorm\NoReturn;
 
 class Response
 {
+    const int SEE_OTHER = 303;
+    const int BAD_REQUEST = 400;
+    const int NOT_FOUND = 404;
+    const int UNAUTHORIZED = 401;
+    const int SERVER_ERROR = 500;
+
     #[NoReturn]
-    public static function abort(): void
+    public static function abort($errorCode = self::NOT_FOUND): void
     {
-        exit('Un problème technique est survenu suite à votre requête');
+        http_response_code($errorCode);
+
+        $path = __DIR__ . "/../resources/views/codes/{$errorCode}.view.php";
+
+        if (file_exists($path)) {
+            include $path;
+        } else {
+            include __DIR__ . "/../resources/views/codes/404.view.php";
+        }
+
+        exit;
     }
 
     #[NoReturn]
