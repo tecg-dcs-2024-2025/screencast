9795a6290660ea65b4c6b0335b9fc3d6be42c344
Dominique Vilain <dominique.vilain@hepl.be>
2025-03-27 11:24:52 +0100

    display pet owner recap

--- app/controllers/LossDeclarationController.php
+++ app/controllers/LossDeclarationController.php
@@ -5,6 +5,7 @@ namespace Animal\Controllers;
 use Animal\Models\Country;
 use Animal\Models\PetOwner;
 use Animal\Models\PetType;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use JetBrains\PhpStorm\NoReturn;
 use Tecgdcs\Response;
 use Tecgdcs\Validator;
@@ -44,16 +45,25 @@ class LossDeclarationController
             update: ['phone']
         );
 
-        //Écrire dans la base de données
+        $pet_owner = PetOwner::latest('updated_at')->first();
 
-        Response::redirect('/loss-declaration');
+        Response::redirect('/loss-declaration?id='.$pet_owner->id);
     }
 
     public function show()
     {
-        // Analyser la query string pour savoir quelle déclaration afficher
+        if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+            Response::abort(Response::BAD_REQUEST);
+        }
+        // Si vous êtes très très inquiet, mais le code avant fait les vérifications nécessaires
+        $id = (int) trim($_GET['id']);
 
-        $email = 'toto';
+        try {
+            $pet_owner = PetOwner::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            Response::abort();
+        }
+        // Analyser la query string pour savoir quelle déclaration afficher
         require VIEW_DIR.'/lossdeclaration/show.php';
     }
 }
